60498e725710705028ca3cea4d83b76bcf11c515
Francisco Viana <franciscosantos29@gmail.com>
2025-12-02 16:06:35 -0300

    Deploy back no render

--- stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/domain/model/ControleFaturamento.java
+++ stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/domain/model/ControleFaturamento.java
@@ -20,7 +20,7 @@ public class ControleFaturamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, columnDefinition = "datetime")
+    @Column(nullable = false)
     private OffsetDateTime emissao;
 
     @ManyToOne
