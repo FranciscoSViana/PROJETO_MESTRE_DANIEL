c7d35995fdae4386b7ea8e389c0200c7765af077
Francisco Viana <franciscosantos29@gmail.com>
2025-12-04 16:19:33 -0300

    Feat: implementando login, autenticação e autorização e cadastro

--- stm-servicos-front/src/environments/environment.development.ts
+++ stm-servicos-front/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl: 'http://localhost:8080'
+    // apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://back-guardian-8ajm.onrender.com'
 };
--- stm-servicos-front/src/environments/environment.production.ts
+++ stm-servicos-front/src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080'
+    // apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://back-guardian-8ajm.onrender.com'
 };
