c26f9dc016e88c6dafb3846163f2e6706cb8d6bc
Francisco Viana <franciscosantos29@gmail.com>
2025-12-08 13:11:32 -0300

    Feat: implementando deploy para o gcp

--- stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/api/config/web/CorsConfig.java
+++ stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/api/config/web/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "https://projeto-mestre-daniel.vercel.app", "https://guardian-front-356067300836.southamerica-east1.run.app"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
--- stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/api/config/web/WebConfig.java
+++ stm-servicos/src/main/java/io/github/franciscosviana/stmservicos/api/config/web/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedOrigins("https://projeto-mestre-daniel.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
